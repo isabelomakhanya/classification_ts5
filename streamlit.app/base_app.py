358e009a7681b8761c1f74fb58142bf4f6397305
isabelomakhanya <isabelomakhanya@gmail.com>
2021-06-23 03:47:16 +0200

    fixed all indentation errors

--- streamlit.app/base_app.py
+++ streamlit.app/base_app.py
@@ -59,6 +59,8 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import seaborn as sns 
 
+import numpy as np
+
 # Vectorizer
 news_vectorizer = open("resources/models/tfidfvect.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
@@ -174,61 +176,61 @@ def main():
 		option = st.selectbox("Choose Option", choices)
         
 		#loading pickle file for the selected model
-        def load_pickle(pkl_file):
-			predictor = joblib.load(open(os.path.join(pkl_file),"rb"))
-			return predictor
+	def load_pickle(pkl_file):
+		predictor = joblib.load(open(os.path.join(pkl_file),"rb"))
+		return predictor
 
-		if option == 'Single Tweet':
+	if option == 'Single Tweet':
 			
-			st.subheader('Classify single tweet')
-            # Creating a text box for user input
-			tweet_text1 = st.text_area("Enter Tweet","Type Here")
+		st.subheader('Classify single tweet')
+		# Creating a text box for user input
+		tweet_text1 = st.text_area("Enter Tweet","Type Here")
 
-            #labels for outcome
-			dict_labels = {1:'Positive', 0:'Neutral', -1:'Negative', 2:'News'}
+		#labels for outcome
+		dict_labels = {1:'Positive', 0:'Neutral', -1:'Negative', 2:'News'}
 
-			#selection for model to be used
-			models1 = ['LinearSVC', 'KNeighborsClassifier','DecisionTreeClassifier', 
-			'RandomForestClassifier', 'ComplementNB', 'MultinomialNB',
-			'AdaBoostClassifier']
-			model1 = st.selectbox("Choose Model", models1)
+		#selection for model to be used
+		models1 = ['LinearSVC', 'KNeighborsClassifier','DecisionTreeClassifier', 
+		'RandomForestClassifier', 'ComplementNB', 'MultinomialNB',
+		'AdaBoostClassifier']
+		model1 = st.selectbox("Choose Model", models1)
 
-			if st.button("Classify"):
-				# Transforming user input with vectorizer
-				clean_text = preprocess_stemm(tweet_text1)
-				vect_text = tweet_cv.transform([clean_text]).toarray()
-		        
-				if model1 == 'LinearSVC':
-					predictor = load_pickle("resources/models/LinearSVC.pkl")
-					prediction = predictor.predict(vect_text)
+		if st.button("Classify"):
+			# Transforming user input with vectorizer
+			clean_text = preprocess_stemm(tweet_text1)
+			vect_text = tweet_cv.transform([clean_text]).toarray()
+			
+			if model1 == 'LinearSVC':
+				predictor = load_pickle("resources/models/LinearSVC.pkl")
+				prediction = predictor.predict(vect_text)
 
-			    elif model1 == 'KNeighborsClassifier':
-					predictor = load_pickle("resources/models/KNeighborsClassifier.pkl")
-					prediction = predictor.predict(vect_text)
+			elif model1 == 'KNeighborsClassifier':
+				predictor = load_pickle("resources/models/KNeighborsClassifier.pkl")
+				prediction = predictor.predict(vect_text)
 
-				elif model1 == 'DecisionTreeClassifier':
-					predictor = load_pickle("resources/models/DecisionTreeClassifier.pkl")
-					prediction = predictor.predict(vect_text)
+			elif model1 == 'DecisionTreeClassifier':
+				predictor = load_pickle("resources/models/DecisionTreeClassifier.pkl")
+				prediction = predictor.predict(vect_text)
 
-				elif model1 == 'RandomForestClassifier':
-					predictor = load_pickle("resources/models/RandomForestClassifier.pkl")
-					prediction = predictor.predict(vect_text)
+			elif model1 == 'RandomForestClassifier':
+				predictor = load_pickle("resources/models/RandomForestClassifier.pkl")
+				prediction = predictor.predict(vect_text)
 
-				elif model1 == 'ComplementNB':
-					predictor = load_pickle("resources/models/ComplementNB.pkl")
-					prediction = predictor.predict(vect_text)
+			elif model1 == 'ComplementNB':
+				predictor = load_pickle("resources/models/ComplementNB.pkl")
+				prediction = predictor.predict(vect_text)
 
-				elif model1  =='MultinomialNB':
-					predictor = load_pickle("resources/models/MultinomialNB.pkl")
-					prediction = predictor.predict(vect_text)
+			elif model1  =='MultinomialNB':
+				predictor = load_pickle("resources/models/MultinomialNB.pkl")
+				prediction = predictor.predict(vect_text)
 
-				elif model1 == 'AdaBoostClassifier':
-					predictor = load_pickle("resources/models/AdaBoostClassifier.pkl")
-					prediction = predictor.predict(vect_text)
-				
-				
-                final_pred = dict_labels[prediction]
-				st.success("Text Categorized as: {}".format(final_pred))
+			elif model1 == 'AdaBoostClassifier':
+				predictor = load_pickle("resources/models/AdaBoostClassifier.pkl")
+				prediction = predictor.predict(vect_text)
+			
+			
+			final_pred = dict_labels[prediction]
+			st.success("Text Categorized as: {}".format(final_pred))
 
 		elif option == "Data set":
 
@@ -254,33 +256,33 @@ def main():
 					predictor = load_pickle("resources/models/LinearSVC.pkl")
 					prediction = predictor.predict(vect_text)
 
-			    elif model2 == 'KNeighborsClassifier':
-					predictor = load_pickle("resources/models/KNeighborsClassifier.pkl")
-					prediction = predictor.predict(vect_text)
+			elif model2 == 'KNeighborsClassifier':
+				predictor = load_pickle("resources/models/KNeighborsClassifier.pkl")
+				prediction = predictor.predict(vect_text)
 
-				elif model2 == 'DecisionTreeClassifier':
-					predictor = load_pickle("resources/models/DecisionTreeClassifier.pkl")
-					prediction = predictor.predict(vect_text)
+			elif model2 == 'DecisionTreeClassifier':
+				predictor = load_pickle("resources/models/DecisionTreeClassifier.pkl")
+				prediction = predictor.predict(vect_text)
 
-				elif model2 == 'RandomForestClassifier':
-					predictor = load_pickle("resources/models/RandomForestClassifier.pkl")
-					prediction = predictor.predict(vect_text)
+			elif model2 == 'RandomForestClassifier':
+				predictor = load_pickle("resources/models/RandomForestClassifier.pkl")
+				prediction = predictor.predict(vect_text)
 
-				elif model2 == 'ComplementNB':
-					predictor = load_pickle("resources/models/ComplementNB.pkl")
-					prediction = predictor.predict(vect_text)
+			elif model2 == 'ComplementNB':
+				predictor = load_pickle("resources/models/ComplementNB.pkl")
+				prediction = predictor.predict(vect_text)
 
-				elif model2  =='MultinomialNB':
-					predictor = load_pickle("resources/models/MultinomialNB.pkl")
-					prediction = predictor.predict(vect_text)
+			elif model2  =='MultinomialNB':
+				predictor = load_pickle("resources/models/MultinomialNB.pkl")
+				prediction = predictor.predict(vect_text)
 
-				elif model2 == 'AdaBoostClassifier':
-					predictor = load_pickle("resources/models/AdaBoostClassifier.pkl")
-					prediction = predictor.predict(vect_text)
-				
-				
-                final_pred2 = dict_labels2[prediction]
-				st.success("Text Categorized as: {}".format(final_pred2))
+			elif model2 == 'AdaBoostClassifier':
+				predictor = load_pickle("resources/models/AdaBoostClassifier.pkl")
+				prediction = predictor.predict(vect_text)
+			
+			
+			final_pred2 = dict_labels2[prediction]
+			st.success("Text Categorized as: {}".format(final_pred2))
 
 
 	elif selection == "EDA":
@@ -288,75 +290,75 @@ def main():
 		df = raw.copy()
 		# Labeling the target
 
-        df['class_label'] = [['Negative(-1)', 'Neutral(0)', 'Positive(1)', 'News(2)'][x+1] for x in df['sentiment']]
-		dist = df.groupby('class_label').count()['clean_message'].reset_index().sort_values(by='clean_message',ascending=False)
+	df['class_label'] = [['Negative(-1)', 'Neutral(0)', 'Positive(1)', 'News(2)'][x+1] for x in df['sentiment']]
+	dist = df.groupby('class_label').count()['clean_message'].reset_index().sort_values(by='clean_message',ascending=False)
 		
-		st.dataframe(df.head())
+	st.dataframe(df.head())
         
 		#bar plot of the count of each sentiment
-		plt.figure(figsize=(12,6))
-		sns.countplot(x='sentiment',data=df, palette='Blues_d')
-		plt.title('Count of Sentiments')
-		st.pyplot()
+	plt.figure(figsize=(12,6))
+	sns.countplot(x='sentiment',data=df, palette='Blues_d')
+	plt.title('Count of Sentiments')
+	st.pyplot()
 
-		# average length of words overall
-        df['clean_message'].str.split().\
-            apply(lambda x : [len(i) for i in x]).\
-            map(lambda x : np.mean(x)).hist()
-        plt.title('Avg number of words used per tweet')
-        plt.xlabel('Number of words per tweet')
-        plt.ylabel('Count of Tweets')
+	# average length of words overall
+	df['clean_message'].str.split().\
+		apply(lambda x : [len(i) for i in x]).\
+		map(lambda x : np.mean(x)).hist()
+	plt.title('Avg number of words used per tweet')
+	plt.xlabel('Number of words per tweet')
+	plt.ylabel('Count of Tweets')
 		
-		# distribution of each of the length of the tweets
-	    df['length_tweet'] = df['clean_message'].apply(len)
-		h = sns.FacetGrid(df,col = 'class_label')
-		h.map(plt.hist,'length_tweet')
-		plt.show()
+	# distribution of each of the length of the tweets
+	df['length_tweet'] = df['clean_message'].apply(len)
+	h = sns.FacetGrid(df,col = 'class_label')
+	h.map(plt.hist,'length_tweet')
+	plt.show()
 
-		#Box plot visual of distribution between length of tweet vs class label
-        plt.figure(figsize=(10, 6))
-        sns.boxplot(x=df['class_label'], 
-		y=df.clean_message.str.split().apply(len),
-		data=df,
-		palette="Blues")
+	#Box plot visual of distribution between length of tweet vs class label
+	plt.figure(figsize=(10, 6))
+	sns.boxplot(x=df['class_label'], 
+	y=df.clean_message.str.split().apply(len),
+	data=df,
+	palette="Blues")
 
-        plt.title('No of Words per Tweet by Sentiment Class')
-        plt.xlabel('Sentiment Class')
-        plt.ylabel('Word Count per Tweet');
+	plt.title('No of Words per Tweet by Sentiment Class')
+	plt.xlabel('Sentiment Class')
+	plt.ylabel('Word Count per Tweet');
 
-        # funnel chart of proportion of each sentiment
-        fig = go.Figure(go.Funnelarea(
-        text = dist.class_label,
-        values = dist.clean_message,
-        title = {"position": "top center", "text": "Funnel-Chart of Sentiment Distribution"}))
-        fig.show()
+	# funnel chart of proportion of each sentiment
+	fig = go.Figure(go.Funnelarea(
+	text = dist.class_label,
+	values = dist.clean_message,
+	title = {"position": "top center", "text": "Funnel-Chart of Sentiment Distribution"}))
+	fig.show()
 
-        # bar plot for average length of messages by sentiments
-		plt.figure(figsize=(12,6))
-		sns.barplot(x='sentiment', y=df['message'].apply(len) ,data = df, palette='Blues_d')
-		plt.ylabel('avg_Length')
-		plt.xlabel('Sentiment')
-		plt.title('Average Length of Message by Sentiment')
-        st.pyplot()
+	# bar plot for average length of messages by sentiments
+	plt.figure(figsize=(12,6))
+	sns.barplot(x='sentiment', y=df['message'].apply(len) ,data = df, palette='Blues_d')
+	plt.ylabel('avg_Length')
+	plt.xlabel('Sentiment')
+	plt.title('Average Length of Message by Sentiment')
+	st.pyplot()
 
-        # most common words in tweet messages
-		fig = px.bar(new, x="count", y="Common_words", color_discrete_sequence =['']*len(df), title='Commmon Words in tweet messages', orientation='h', 
-             width=600, height=600)
-        fig.show()
+	# most common words in tweet messages
+	fig = px.bar(new, x="count", y="Common_words", color_discrete_sequence =['']*len(df), title='Commmon Words in tweet messages', orientation='h', 
+			width=600, height=600)
+	fig.show()
 		
 		
-    elif selection == 'Lets connect!':
+elif: selection == 'Lets connect'
 
-		st.subheader("Have questions? We are an email away to answer your questions")
+st.subheader("Have questions? We are an email away to answer your questions")
 
-		st.write("Noxolo: wendyngcobo98@gmail.com")
-		st.write("Sabelo: isabelomakhanya@gmail.com")
-		st.write("Morgan: letlhogonolomorgan69@gmail.com")
-		st.write("Tebogo: mrtjsambo@gmail.com")
-		st.write("Sergio: sergiomornayseptember@gmail.com")
-		st.write("Vuyelwa: vuyelwaf22@gmail.com")
-		image = Image.open('resources/imgs/EDSA_logo.png')
-		st.image(image, caption='TS5_EDSA_2021', use_column_width=True)
+st.write("Noxolo: wendyngcobo98@gmail.com")
+st.write("Sabelo: isabelomakhanya@gmail.com")
+st.write("Morgan: letlhogonolomorgan69@gmail.com")
+st.write("Tebogo: mrtjsambo@gmail.com")
+st.write("Sergio: sergiomornayseptember@gmail.com")
+st.write("Vuyelwa: vuyelwaf22@gmail.com")
+image = Image.open('resources/imgs/EDSA_logo.png')
+st.image(image, caption='TS5_EDSA_2021', use_column_width=True)
 
 
     #st.write()
